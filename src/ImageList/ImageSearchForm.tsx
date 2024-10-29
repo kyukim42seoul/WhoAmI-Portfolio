c9a394548799522abdbb356f90629cf06250dc7b
kyukim42seoul <kyukim@student.42seoul.kr>
2024-10-29 17:16:02 +0900

    Use fixed base_url

--- src/ImageList/ImageSearchForm.tsx
+++ src/ImageList/ImageSearchForm.tsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import styles from "./ImageSearchForm.module.css";
 //import { BASE_URL } from "../constants.ts";
 
-const BASE_URL = import.meta.env.VITE_REQ_TO;
-console.log(`URL: ${BASE_URL}`, BASE_URL);
+//const BASE_URL = import.meta.env.VITE_REQ_TO;
 
 interface Props {
   setQuery: React.Dispatch<React.SetStateAction<string>>;
@@ -16,7 +15,11 @@ const ImageSearchForm = ({ setQuery }: Props) => {
   const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const seacrhURL =
-      BASE_URL + "search?query=" + searchParam + "&per_page=" + photoCount;
+      "https://api.pexels.com/v1/" +
+      "search?query=" +
+      searchParam +
+      "&per_page=" +
+      photoCount;
     if (searchParam && photoCount) {
       setQuery(seacrhURL);
     }
